5cbfa2922c96dbf4f9406731bc647f9664d65d20
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-08 21:55:02 +0200

    add: filemanager class for get/put

---
+++ filemanager.py
@@ -0,0 +1,26 @@
+import simplejson
+import json
+
+def put(data, filename):
+	try:
+		jsondata = simplejson.dumps(data, indent=4, skipkeys=True, sort_keys=True)
+		fd = open(filename, 'w')
+		fd.write(jsondata)
+		fd.close()
+	except:
+		print('ERROR writing', filename)
+		pass
+
+def get(filename):
+	returndata = {}
+	try:
+        # filename = 'dink.json'
+		fd = open('./'+filename, 'r')
+		text = fd.read()
+		fd.close()
+		returndata = json.loads(text)
+		# Hm.  this returns unicode keys...
+		#returndata = simplejson.loads(text)
+	except:
+		print('COULD NOT LOAD:', filename)
+	return returndata
