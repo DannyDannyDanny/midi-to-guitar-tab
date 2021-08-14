6785ee9cfb42f1089f3ac998eea804ef30cfb950
DannyDannyDanny <dtho>
2021-08-14 14:37:20 +0200

    :recycle: add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='midi-to-guitar-tab',
+    version='0.0.1',
+    long_description=__doc__,
+    packages=['src'],
+    url='https://github.com/DannyDannyDanny/midi-to-guitar-tab/',
+    author='Daniel Thoren',
+    author_email='danielth95@gmail.com',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
