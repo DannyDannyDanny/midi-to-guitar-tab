f13a78b1db83324461f4202c3262d2e4e0812cc4
DannyDannyDanny <dtho>
2021-08-14 14:58:26 +0200

    :art: aplit src, test and sample

--- sample/moodoo_dev.py
+++ sample/moodoo_dev.py
@@ -1,8 +1,7 @@
 import mido
 from mido import MidiFile
-from pynput.keyboard import Key, Listener
 import os
-from moodoo import moodoo
+from src.moodoo import moodoo, generate_string_list
 
 note = 0
 
@@ -27,25 +26,6 @@ for i in zip(notelist,vislist):
 
 # %%
 m = moodoo('midifiles/sad_rude2.mid')
-def generate_string_list(m,visvar):
-    notelist = m.get_notelist()
-    notebaglist = m.get_note_bag_list()
-    vislist = m.note_bag_list_to_visual(notebaglist)
-    # visvar = [1 for vis in vislist] # <---- edit this to change variations
-
-    string_list = []
-
-    string_list.append('octave\t|note\t|playable(string,fret)\t|variation')
-    for i,(visnote,(oct,note)) in enumerate(zip(vislist,notelist)):
-        playable = m.note_dict[note,oct+1] #TODO
-        variation = visvar[i]
-        note_vis = visnote[variation]
-        v = 1
-        # variatin number string i.e [1/3]
-        varnostr = '['+str(v)+'/'+str(len(visnote))+']'
-        # print(oct,'\t',note,'\t',playable)
-        string_list.append(str(oct)+'\t\t|'+str(note)+'\t'+note_vis+'\t\t|'+varnostr)
-    return string_list
 
 # string_list = generate_string_list(m)
 
--- src/moodoo.py
+++ src/moodoo.py
@@ -1,7 +1,28 @@
 import mido
 from mido import MidiFile
-from pynput.keyboard import Key, Listener
 import os
+
+
+def generate_string_list(m, visvar=None):
+    notelist = m.get_notelist()
+    notebaglist = m.get_note_bag_list()
+    vislist = m.note_bag_list_to_visual(notebaglist)
+    if visvar is None:
+        visvar = [1 for vis in vislist] # <---- edit this to change variations
+
+    string_list = []
+
+    string_list.append('octave\t|note\t|playable(string,fret)\t|variation')
+    for i,(visnote,(oct,note)) in enumerate(zip(vislist,notelist)):
+        playable = m.note_dict[note,oct+1] #TODO
+        variation = visvar[i]
+        note_vis = visnote[variation]
+        v = 1
+        # variatin number string i.e [1/3]
+        varnostr = '['+str(v)+'/'+str(len(visnote))+']'
+        # print(oct,'\t',note,'\t',playable)
+        string_list.append(str(oct)+'\t\t|'+str(note)+'\t'+note_vis+'\t\t|'+varnostr)
+    return string_list
 
 class moodoo:
     # notes in an octave
